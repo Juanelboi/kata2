ebcacbf4b5ca769102b7b2d3544b433afa62ee2e
John Doe <juangarciaboissier@gmail.com>
2024-12-29 17:34:28 +0000

    enum created

---
+++ src/main/java/software/ulpgc/WeaponType.java
@@ -0,0 +1,37 @@
+package software.ulpgc;
+
+public enum WeaponType {
+    GlintstoneStaff,
+    Greatsword,
+    Bow,
+    ThrustingSword,
+    Crossbow,
+    ColossalWeapon,
+    Greataxe,
+    CurvedSword,
+    Halberd,
+    Flail,
+    Axe,
+    Warhammer,
+    Torch,
+    CurvedGreatsword,
+    Dagger,
+    Claw,
+    HeavyThrustingSword,
+    Spear,
+    StraightSword,
+    Fist,
+    SacredSeal,
+    Hammer,
+    LightBow,
+    Katana,
+    Twinblade,
+    Greatbow,
+    Whip,
+    ColossalSword,
+    Reaper,
+    Ballista,
+    GreatSpear
+
+
+}
